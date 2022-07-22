fc6b56fe888aef7b8182614bf549446f6526f74e
Dungna89 <dungna29@fpt.edu.vn>
2022-07-22 11:20:55 +0700

    1

---
+++ 2_IT17331_NET103_BL2_SM22/BAI_1_3_ADO/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAI_1_3_ADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Data Source=DUNGNAPC\SQLEXPRESS;Initial Catalog=FINALASS_FPOLYSHOP_SM22_BL2_DUNGNA29;User ID=dungna35;Password=123456
+        private void btn_KetNoi_Click(object sender, EventArgs e)
+        {
+            string sqlConnectionString = @"Data Source=DUNGNAPC\SQLEXPRESS;Initial Catalog=FINALASS_FPOLYSHOP_SM22_BL2_DUNGNA29;User ID=dungna35;Password=123456";
+            //1. Khởi tạo kết nối vào csdl
+            SqlConnection conn = new SqlConnection(sqlConnectionString);
+            //2. Mở kết nối đến
+            conn.Open();
+            //3. Mở kết nối xóng thì sẽ thực hiện 1 hành động nào đó.
+            //4. Khởi tạo 1 câu query với SqlCommand
+            string sql = "SELECT * FROM ChucVu";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            //5. Thực thi và hứng dữ liệu vào SqlDataAdapter
+            SqlDataAdapter data = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            //6. Sua khi thực hiện xong hành động thì phải đóng kết nối.
+            conn.Close();
+        }
+    }
+}
